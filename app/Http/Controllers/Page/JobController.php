d7c8652b40621ed3a0589e6fe0e3a1015565a1ae
chandara2 <chandara.ict@gmail.com>
2022-02-08 16:20:50 +0700

    NA

--- app/Http/Controllers/Page/JobController.php
+++ app/Http/Controllers/Page/JobController.php
@@ -80,11 +80,11 @@ class JobController extends Controller
     public function jobsort($jobsort)
     {
         $jobscoms = DB::table('jobs')
+            ->join('company_infos', 'company_infos.uid', '=', 'jobs.uid')
             ->join('job_functions', 'job_functions.name', '=', 'jobs.function')
             ->join('job_industries', 'job_industries.name', '=', 'jobs.industry')
             ->join('job_locations', 'job_locations.name', '=', 'jobs.location')
             ->join('job_salaries', 'job_salaries.name', '=', 'jobs.salary')
-            ->join('company_infos', 'company_infos.uid', '=', 'jobs.uid')
             ->select('jobs.*', 'company_infos.*', 'jobs.id as job_id', 'company_infos.id as com_id')
             ->where('job_functions.name', $jobsort)
             ->orWhere('job_industries.name', $jobsort)
